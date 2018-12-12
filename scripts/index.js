9b10f451b1dd0f3bbbad003170d2a388c26cf24b
Feroz Ahmmed <froghramar@gmail.com>
2018-12-12 17:20:04 +0600

    move guid generator to IIFE

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,23 @@
-(function(window){
-    function getGUid() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        });
-    }
+(function(){
 
-    function getGuidList(numberOfGUIDs) {
-        return Array.from(Array(numberOfGUIDs).keys()).map(function () {
-            return getGUid();
-        });
-    }
+    const guidGenerator = (function () {
+        function getGUid() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                const r = Math.random() * 16 | 0, v = (c === 'x') ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        }
+
+        function getGuidList(numberOfGUIDs) {
+            return Array.from(Array(numberOfGUIDs).keys()).map(function () {
+                return getGUid();
+            });
+        }
+
+        return {
+            getGuidList: getGuidList
+        }
+    })();
 
     const oggApp = new Vue({
         el: '#ogg-app',
@@ -20,9 +27,9 @@
         },
         methods: {
             generateBtnClick: function() {
-                const guidList = getGuidList(this.numberOfGUIDs);
+                const guidList = guidGenerator.getGuidList(this.numberOfGUIDs);
                 this.textAreaResult = guidList.join(', ');
             }
         }
     })
-})(window);
+})();
